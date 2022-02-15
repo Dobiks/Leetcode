3bff3da88892b11eecc332ef7d7f7b34ac5ec0d6
Dobiks <49527911+Dobiks@users.noreply.github.com>
2022-02-15 18:34:19 +0100

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -2,6 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i,a in enumerate(nums):
             for j,b in enumerate(nums):
-                if a+b==target:
+                if a+b==target and j != i:
                     return [i,j]
             
