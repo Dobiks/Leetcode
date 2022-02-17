b67f8f4cfe43130b3a4d2102b35c2919e99a61a0
Dobiks <49527911+Dobiks@users.noreply.github.com>
2022-02-17 13:52:54 +0100

    Update palindrome-number.py

--- palindrome-number.py
+++ palindrome-number.py
@@ -2,8 +2,9 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if list(str(x))[::-1]==list(str(x)): return True
         else: return False
-
-
-s = Solution()
-print(s.isPalindrome(x = 129))
-
+        
+    
+'''
+Runtime: 74 ms, faster than 63.38% of Python3 online submissions for Palindrome Number.
+Memory Usage: 13.9 MB, less than 72.36% of Python3 online submissions for Palindrome Number.
+'''
